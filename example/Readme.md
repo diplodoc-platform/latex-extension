ae173a182a1c895003397ac1309ed0a0c1e8b0cf
3y3 <3y3@ya.ru>
2023-12-17 03:58:38 +0300

    Add example

---
+++ example/Readme.md
@@ -0,0 +1,20 @@
+# Simple example
+
+```
+npm i
+npm start
+```
+
+Then this documentation will be rendered by [@diplodoc/transform](https://github.com/diplodoc-platform/transform) and opened in your default browser.
+
+## Latex
+
+### Inline
+
+Some text $c = \pm\sqrt{a^2 + b^2}$
+
+### Block
+
+$$
+c = \pm\sqrt{a^2 + b^2}
+$$
---
+++ example/index.js
@@ -0,0 +1,45 @@
+import transform from '@diplodoc/transform';
+import latex from '@diplodoc/latex-extension';
+import {readFile} from 'node:fs/promises';
+
+(async () => {
+    const content = await readFile('./Readme.md', 'utf8');
+    const {result} = await transform(content, {
+        output: './build',
+        plugins: [latex.transform()],
+    });
+
+    const styles = result.meta.style
+        .map((style) => {
+            return `<link rel="stylesheet" href="${style}" />`;
+        })
+        .join('\n');
+
+    const scripts = result.meta.script
+        .map((script) => {
+            return `<script src="${script}" async></script>`;
+        })
+        .join('\n');
+
+    const html = `
+<html>
+    <head>
+        ${styles}
+        ${scripts}
+        <script>
+            window.latexJsonp = window.latexJsonp || [];
+            window.latexJsonp.push(function(latex) {
+                window.addEventListener('load', function() {
+                    latex.run();
+                });
+            });
+        </script>
+    </head>
+    <body style="background: #FFF">
+        ${result.html}
+    </body>
+</html>
+    `;
+
+    console.log(html);
+})();
